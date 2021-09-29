18aa4ddb40944ec055b2921aa58eb505ba482757
DESKTOP-VI6CUSJ\82107 <rlaalsxo1990@naver.com>
2021-09-29 20:46:01 +0900

    회원 엔티티 추가 및 테스트로직 추가

---
+++ src/main/java/portfolio/shopapi/domain/embedded/Address.java
@@ -0,0 +1,19 @@
+package portfolio.shopapi.domain.embedded;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
